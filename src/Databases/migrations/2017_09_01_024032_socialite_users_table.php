13083229e38e7bad1a5f3c2f3bbfe2bd17c0017b
bigrocs <bigrocs@qq.com>
2017-09-01 16:49:51 +0800

    add ullable

--- src/Databases/migrations/2017_09_01_024032_socialite_users_table.php
+++ src/Databases/migrations/2017_09_01_024032_socialite_users_table.php
@@ -15,16 +15,16 @@ class SocialiteUsersTable extends Migration
     {
         Schema::create('socialite_users', function (Blueprint $table) {
             $table->increments('user_id')->unsigned();
-            $table->integer('github')->comment('GitHub')->unsigned();
-            $table->integer('facebook')->comment('Facebook')->unsigned();
-            $table->integer('twitter')->comment('Twitter')->unsigned();
-            $table->integer('linkedin')->comment('LinkedIn')->unsigned();
-            $table->integer('qq')->comment('QQ')->unsigned();
-            $table->integer('weixin')->comment('Weixin')->unsigned();
-            $table->integer('weixinweb')->comment('WeixinWeb')->unsigned();
-            $table->integer('weibo')->comment('Weibo')->unsigned();
-            $table->integer('taobao')->comment('TaoBao')->unsigned();
-            $table->integer('alipay')->comment('Alipay')->unsigned();
+            $table->integer('github')->comment('GitHub')        ->nullable()->unsigned();
+            $table->integer('facebook')->comment('Facebook')    ->nullable()->unsigned();
+            $table->integer('twitter')->comment('Twitter')      ->nullable()->unsigned();
+            $table->integer('linkedin')->comment('LinkedIn')    ->nullable()->unsigned();
+            $table->integer('qq')->comment('QQ')                ->nullable()->unsigned();
+            $table->integer('weixin')->comment('Weixin')        ->nullable()->unsigned();
+            $table->integer('weixinweb')->comment('WeixinWeb')  ->nullable()->unsigned();
+            $table->integer('weibo')->comment('Weibo')          ->nullable()->unsigned();
+            $table->integer('taobao')->comment('TaoBao')        ->nullable()->unsigned();
+            $table->integer('alipay')->comment('Alipay')        ->nullable()->unsigned();
             $table->foreign('user_id')->references('id')->on('core_users')
                 ->onUpdate('cascade')->onDelete('cascade');
         });
