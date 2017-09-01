541e0a2bda6525e69115826573890ac14db037a0
bigrocs <bigrocs@qq.com>
2017-09-01 14:00:54 +0800

    注册配置文件 注册服务

--- src/SocialiteServiceProvider.php
+++ src/SocialiteServiceProvider.php
@@ -3,7 +3,7 @@
 namespace CoreCMF\Socialite;
 
 use Illuminate\Support\ServiceProvider;
-
+use Socialite;
 class SocialiteServiceProvider extends ServiceProvider
 {
     /**
@@ -11,9 +11,14 @@ class SocialiteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Socialite $Socialite)
     {
-      print_r('aaa');
+        //配置路由
+        $this->loadRoutesFrom(__DIR__.'/Routes/web.php');
+        // 加载配置
+        $this->mergeConfigFrom(__DIR__.'/Config/config.php', 'socialite');
+        $this->initService();
+
     }
 
     /**
@@ -24,5 +29,29 @@ class SocialiteServiceProvider extends ServiceProvider
     public function register()
     {
 
+    }
+    /**
+     * 初始化服务
+     */
+    public function initService()
+    {
+        $github = [
+            'client_id' => '8aeca72d03782ea999ad',
+            'client_secret' => '68b41095d1723798590ed3f0452a9c91dec92e05',
+            'redirect' => 'http://corecmf.dev/OAuth/github/callback',
+        ];
+        config(['services.github' => $github]);
+        //注册providers服务
+        $this->registerProviders();
+    }
+    /**
+     * 注册引用服务
+     */
+    public function registerProviders()
+    {
+        $providers = config('socialite.providers');
+        foreach ($providers as $provider) {
+            $this->app->register($provider);
+        }
     }
 }
