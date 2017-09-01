9e79a21d7453924c0ba990db02740c5bc9bb0399
bigrocs <bigrocs@qq.com>
2017-09-01 17:34:51 +0800

    创建用户 绑定用户 重定向写台cookie

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -2,76 +2,83 @@
 
 namespace CoreCMF\Socialite\Http\Controllers;
 
+use Auth;
 use Socialite;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Laravel\Passport\ApiTokenCookieFactory;
 use CoreCMF\Socialite\Models\User as SocialiteUser;
 use CoreCMF\Core\Models\User;
 
 class AuthController extends Controller
 {
     private $request;
+    private $cookieFactory;
     private $userModel;
     private $socialiteUserModel;
 
-    public function __construct(Request $request, User $userRepo, SocialiteUser $socialiteUserRepo)
+    public function __construct(
+      Request $request,
+      ApiTokenCookieFactory $cookieFactory,
+      User $userRepo,
+      SocialiteUser $socialiteUserRepo
+    )
     {
         $this->request = $request;
+        $this->cookieFactory = $cookieFactory;
         $this->userModel = $userRepo;
         $this->socialiteUserModel = $socialiteUserRepo;
     }
     /**
-     * 将用户重定向到Github认证页面
+     * 将用户重定向到Provider认证页面
      *
      * @return Response
      */
     public function redirectToProvider($service, $redirect=null)
     {
-
-        $this->socialiteUserModel->$service = 123;
-        $this->socialiteUserModel->user_id  = 6;
-        dd($this->socialiteUserModel->save());
         session(['redirect' => $redirect]);//传入授权后的重定向加密网址 存入session
         return Socialite::driver($service)->with(['hd' => 'example.com'])->redirect();
     }
 
     /**
-     * 从Github获取用户信息.
+     * 从Provider获取用户信息.
      *
      * @return Response
      */
     public function handleProviderCallback($service)
     {
         $socialiteUser = Socialite::driver($service)->user();
-        $users = $this->socialiteUserModel->where($service, $socialiteUser->id)->with('users')->first();
-        if (empty($users)) {
-            $this->createUser($service,$socialiteUser);
+        $socialite = $this->socialiteUserModel->where($service, $socialiteUser->id)->with('users')->first();
+        if (empty($socialite)) {
+            $user = $this->createUser($service,$socialiteUser);//创建用户
+        }else{
+            $user = $socialite->users;
         }
-        dd($users);
-        return redirect($this->redirectUrl());//重定向
+        $cookie = $this->cookieFactory->make(
+            Auth::id(), $this->request->session()->token()
+        );//携带 api  cookie
+        Auth::login($user);
+        return redirect($this->redirectUrl())->withCookie($cookie);//重定向
     }
     /**
      * 创建用户
      */
     private function createUser($service,$socialiteUser)
     {
-        // $this->userModel->github = $socialiteUser->id;
-        // $userModel->email = $user->email;
-        // $userModel->name = $user->name;
-        // $userModel->avatar = $user->avatar;
-        // $this->userModel->save();
-        // $this->userModel->email = $socialiteUser->email;
-        $input = [
-            'name' => $socialiteUser->name,
-            'email' => $socialiteUser->email,
-            'password' => str_random(10),//随机密码
-            'nickname' => $socialiteUser->nickname,
-        ];
-        $user = $this->userModel->create($input);
+        $user = Auth::user();//获取当前用户 如果没有用户注册新用户
+        if (!$user) {
+            $input = [
+                'name' => $socialiteUser->name,
+                'email' => $socialiteUser->email,
+                'password' => str_random(10),//随机密码
+                'nickname' => $socialiteUser->nickname,
+            ];
+            $user = $this->userModel->create($input);//创建用户
+        }
+        // 关联用户
         $this->socialiteUserModel->$service = $socialiteUser->id;
         $this->socialiteUserModel->user_id  = $user->id;
-        $this->socialiteUserModel->save();
-        dd($socialiteUser,$user);
+        return $this->socialiteUserModel->save()? $user: false;
     }
     /**
      * 重定向url
