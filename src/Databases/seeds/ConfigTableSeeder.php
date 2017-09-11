3b54f509fcb5a4b719354f43df181cf3ab382546
bigrocs <bigrocs@qq.com>
2017-09-11 10:43:01 +0800

    没有驱动的先注释掉

--- src/Databases/seeds/ConfigTableSeeder.php
+++ src/Databases/seeds/ConfigTableSeeder.php
@@ -33,16 +33,31 @@ class ConfigTableSeeder extends Seeder
             'service' => 'weibo',
             'redirect'=> 'http://corecmf.dev/OAuth/weibo/callback',
         ]);
+        // DB::table('socialite_configs')->insert([
+        //     'name' => '人人',
+        //     'service' => 'renren',
+        //     'redirect'=> 'http://corecmf.dev/OAuth/renren/callback',
+        // ]);
         DB::table('socialite_configs')->insert([
             'name' => '豆瓣',
             'service' => 'douban',
             'redirect'=> 'http://corecmf.dev/OAuth/douban/callback',
         ]);
-        DB::table('socialite_configs')->insert([
-            'name' => '百度',
-            'service' => 'baidu',
-            'redirect'=> 'http://corecmf.dev/OAuth/baidu/callback',
-        ]);
+        // DB::table('socialite_configs')->insert([
+        //     'name' => '百度',
+        //     'service' => 'baidu',
+        //     'redirect'=> 'http://corecmf.dev/OAuth/baidu/callback',
+        // ]);
+        // DB::table('socialite_configs')->insert([
+        //     'name' => '支付宝',
+        //     'service' => 'alipay',
+        //     'redirect'=> 'http://corecmf.dev/OAuth/alipay/callback',
+        // ]);
+        // DB::table('socialite_configs')->insert([
+        //     'name' => '淘宝',
+        //     'service' => 'taobao',
+        //     'redirect'=> 'http://corecmf.dev/OAuth/taobao/callback',
+        // ]);
         DB::table('socialite_configs')->insert([
             'name' => 'GitHub',
             'service' => 'github',
