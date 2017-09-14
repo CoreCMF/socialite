441fd2d824e590cfbc1e769cd8b648521613e5bb
bigrocs <bigrocs@qq.com>
2017-09-14 17:24:54 +0800

    upadte

--- src/Config/config.php
+++ src/Config/config.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Socialite',
-    'title' => '社会第三方登录插件',
+    'title' => '社会第三方登录',
     'description' => '社会第三方登录插件,引用Laravel\Socialite服务,可以实现QQ、微信、新浪、淘宝、支付宝、github等主流第三方登录。',
     'author' => 'BigRocs',
     'version' => 'v1.1.6',
