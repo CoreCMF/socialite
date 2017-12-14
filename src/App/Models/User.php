4f65acec98463a31d56b687e52a907159d24d562
bigrocs <bigrocs@qq.com>
2017-12-14 16:32:25 +0800

    getSocialiteId

--- src/App/Models/User.php
+++ src/App/Models/User.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace CoreCMF\Socialite\App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
@@ -11,6 +12,20 @@ class User extends Model
 
     public function users()
     {
-        return $this->belongsTo(\CoreCMF\Core\App\Models\User::class,'user_id');
+        return $this->belongsTo(\CoreCMF\Core\App\Models\User::class, 'user_id');
+    }
+    /**
+     * [getSocialiteId 根据驱动获取当前用户唯一ID]
+     * @param    [type]         $userId  [description]
+     * @param    [type]         $service [description]
+     * @return   [type]                  [description]
+     * @Author   bigrocs
+     * @QQ       532388887
+     * @Email    bigrocs@qq.com
+     * @DateTime 2017-12-14
+     */
+    public function getSocialiteId($userId, $service)
+    {
+        return $this->where('user_id', $userId)->first()->$service;
     }
 }
