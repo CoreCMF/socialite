07fbc96c08b5924373db9dc03f4b2836e6d3a63b
bigrocs <bigrocs@qq.com>
2018-02-04 13:49:46 +0800

    引入vue

--- resources/views/scanLogin.blade.php
+++ resources/views/scanLogin.blade.php
@@ -12,44 +12,10 @@
         <script src="http://cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="http://cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href={{ asset('/vendor/'.$model.'/css/app.min.css') }} rel=stylesheet>
-    <!-- 渲染插件Css -->
-@if (!empty($resources['css']))
-  @foreach ($resources['css'] as $css)
-  <link href={{ $css }} rel=stylesheet>
-  @endforeach
-@endif
-    <script>
-        window.config = {
-            apiUrl: '/api/{{$model}}/main',
-            csrfToken:'{{ csrf_token() }}',
-            userId: {{ empty(Auth::id())? 0: Auth::id()  }},
-@if (!empty($resources['config']))
-  @foreach ($resources['config'] as $key => $config)
-          {{ $key }}: {!! json_encode($config) !!},
-  @endforeach
-@endif
-        }
-    </script>
+    <link href={{ asset('/vendor/socialite/css/app.css') }} rel=stylesheet>
 </head>
 <body>
     <div id=app></div>
-    <!-- 渲染插件Html Begin -->
-@if (!empty($resources['html']))
-  @foreach ($resources['html'] as $html)
-  {!! $html !!}
-  @endforeach
-@endif
-    <!-- 渲染插件Html End -->
-    <!-- 渲染插件Js Begin -->
-@if (!empty($resources['js']))
-  @foreach ($resources['js'] as $js)
-  <script type=text/javascript src={{ $js }}></script>
-  @endforeach
-@endif
-  <!-- 渲染插件Js End -->
-    <script type=text/javascript src={{ asset('/vendor/'.$model.'/js/manifest.min.js') }}></script>
-    <script type=text/javascript src={{ asset('/vendor/'.$model.'/js/vendor.min.js') }}></script>
-    <script type=text/javascript src={{ asset('/vendor/'.$model.'/js/app.min.js') }}></script>
+    <script type=text/javascript src={{ asset('/vendor/js/vue.js') }}></script>
 </body>
 </html>
