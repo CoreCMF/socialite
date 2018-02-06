20ed45e375943eb41b82516039775b65c492d9bd
bigrocs <bigrocs@qq.com>
2018-02-06 15:57:00 +0800

    更改路由错误 防止访问出错

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -17,7 +17,7 @@
 */
 Route::group(['prefix' => 'OAuth', 'middleware' => 'web', 'namespace' => 'CoreCMF\Socialite\App\Http\Controllers', 'as' => 'OAuth.'], function () {
     Route::get('{service}/callback', [ 'as' => 'callback', 'uses' => 'AuthController@handleProviderCallback']);
-    Route::get('Scan/{redirect?}', [ 'as' => 'Scan', 'uses' => 'AuthController@scanLogin']);
     Route::get('Scan/wap/{sessionId?}', [ 'as' => 'Scan.wap', 'uses' => 'AuthController@scanWapLogin']);
+    Route::get('Scan/{redirect?}', [ 'as' => 'Scan', 'uses' => 'AuthController@scanLogin']);
     Route::get('{service}/{redirect?}', [ 'as' => 'redirect', 'uses' => 'AuthController@redirectToProvider']);
 });
